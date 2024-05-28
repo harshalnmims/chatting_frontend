96377d634977ba76f07df0e6b6d9465f193895fd
harshalnmims <harshal.nanekar.EXT@nmims.edu>
2024-05-28 18:53:50 +0530

    first commit

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+let userId;
+
+</script>
+
+<div class="bg-white">
+
+<div class="">
+<label for="userId"  >Enter Email-Id/Phone Number</label>
+<input type="text" id="userId" bind:value= {userId} />
+</div>
+
+
+</div>
+
+
